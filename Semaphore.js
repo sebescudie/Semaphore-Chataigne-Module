5385cb088a56e944dedf71d4137df17958c29059
sebescudie <ping@sebescudie.fr>
2025-10-29 18:18:26 +0100

    Adds Limit parameter to startTask
    On Semaphore, the task template needs to have "Limit" checked in the template definition. Otherwise, a limit flag provided from the outside won't be taken into account

--- Semaphore.js
+++ Semaphore.js
@@ -1,10 +1,22 @@
-function startTask(projectId, templateId) {
+function startTask(projectId, templateId, limit) {
     var payload = {};
     payload.template_id = templateId;
+    payload.limit = limit;
     
     var params = {};
-    params.extraHeaders = "Authorization: Bearer " + local.parameters.getChild("token").get() + "\rContent-Type: application/json" + "\rAccept: application/json, text/plain, */*";
+    params.extraHeaders = formatHeaders(local.parameters.getChild("token").get());
     params.payload = payload;
 
-    local.sendPOST("project/" + projectId + "/tasks", params);
+    local.sendPOST("/project/" + projectId + "/tasks", params);
+}
+
+function getTaskOutput(projectId, taskId){
+    var params = {};
+    params.extraHeaders = formatHeaders(local.parameters.getChild("token").get());
+    
+    local.sendGET("project/" + projectId + "/tasks/" + taskId, params);
+}
+
+function formatHeaders(token){
+    return "Authorization: Bearer " + token + "\rContent-Type: application/json" + "\rAccept: application/json, text/plain, */*";
 }
